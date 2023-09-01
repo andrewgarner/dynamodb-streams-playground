06c8464388ffeef9f57b096dddb2fd8a8543f2c4
Andrew Garner <andrew.garner@cazoo.co.uk>
2023-09-01 16:42:55 +0100

    Basic DynamoDB stream handler

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,108 @@
 import { describe, expect, it } from "@jest/globals";
-import { dynamoDBStream } from "./index.js";
+import { DynamoDBStreamEvent } from "aws-lambda";
+import { ContextExamples } from "@aws-lambda-powertools/commons";
+import { getProcessedItems, handler } from "./index.js";
 
-describe("dynamoDB Streams", () => {
-  it("works", () => {
-    expect(dynamoDBStream).toBe("DynamoDB Stream");
+describe("handler", () => {
+  it("handles a DynamoDB stream event", async () => {
+    const event: DynamoDBStreamEvent = {
+      Records: [
+        {
+          eventID: "c4ca4238a0b923820dcc509a6f75849b",
+          eventName: "INSERT",
+          eventVersion: "1.1",
+          eventSource: "aws:dynamodb",
+          awsRegion: "eu-west-1",
+          dynamodb: {
+            Keys: {
+              Id: {
+                N: "101",
+              },
+            },
+            NewImage: {
+              Message: {
+                S: "New item!",
+              },
+              Id: {
+                N: "101",
+              },
+            },
+            ApproximateCreationDateTime: 1428537600,
+            SequenceNumber: "4421584500000000017450439091",
+            SizeBytes: 26,
+            StreamViewType: "NEW_AND_OLD_IMAGES",
+          },
+          eventSourceARN: "arn:aws:dynamodb:eu-west-1:123456789012:table/playground/stream/2015-06-27T00:48:05.899",
+        },
+        {
+          eventID: "c81e728d9d4c2f636f067f89cc14862c",
+          eventName: "MODIFY",
+          eventVersion: "1.1",
+          eventSource: "aws:dynamodb",
+          awsRegion: "eu-west-1",
+          dynamodb: {
+            Keys: {
+              Id: {
+                N: "101",
+              },
+            },
+            NewImage: {
+              Message: {
+                S: "This item has changed",
+              },
+              Id: {
+                N: "101",
+              },
+            },
+            OldImage: {
+              Message: {
+                S: "New item!",
+              },
+              Id: {
+                N: "101",
+              },
+            },
+            ApproximateCreationDateTime: 1428537600,
+            SequenceNumber: "4421584500000000017450439092",
+            SizeBytes: 59,
+            StreamViewType: "NEW_AND_OLD_IMAGES",
+          },
+          eventSourceARN: "arn:aws:dynamodb:eu-west-1:123456789012:table/playground/stream/2015-06-27T00:48:05.899",
+        },
+        {
+          eventID: "eccbc87e4b5ce2fe28308fd9f2a7baf3",
+          eventName: "REMOVE",
+          eventVersion: "1.1",
+          eventSource: "aws:dynamodb",
+          awsRegion: "eu-west-1",
+          dynamodb: {
+            Keys: {
+              Id: {
+                N: "101",
+              },
+            },
+            OldImage: {
+              Message: {
+                S: "This item has changed",
+              },
+              Id: {
+                N: "101",
+              },
+            },
+            ApproximateCreationDateTime: 1428537600,
+            SequenceNumber: "4421584500000000017450439093",
+            SizeBytes: 38,
+            StreamViewType: "NEW_AND_OLD_IMAGES",
+          },
+          eventSourceARN: "arn:aws:dynamodb:eu-west-1:123456789012:table/playground/stream/2015-06-27T00:48:05.899",
+        },
+      ],
+    };
+
+    await expect(handler(event, ContextExamples.helloworldContext)).resolves.toMatchObject({ batchItemFailures: [] });
+    await expect(getProcessedItems()).resolves.toMatchObject([
+      { Id: 101, Message: "New item!" },
+      { Id: 101, Message: "This item has changed" },
+    ]);
   });
 });
--- index.ts
+++ index.ts
@@ -1,1 +1,36 @@
-export const dynamoDBStream = "DynamoDB Stream";
+import { injectLambdaContext, Logger } from "@aws-lambda-powertools/logger";
+import middy from "@middy/core";
+import { DynamoDBBatchItemFailure, DynamoDBRecord, DynamoDBStreamHandler } from "aws-lambda";
+import { NativeAttributeValue, unmarshall } from "@aws-sdk/util-dynamodb";
+
+const items: unknown[] = [];
+const logger: Logger = new Logger({ serviceName: "dynamodb-streams-playground" });
+
+const dynamoDBStreamHandler: DynamoDBStreamHandler = async (event) => {
+  const batchItemFailures: DynamoDBBatchItemFailure[] = [];
+
+  for (const record of event.Records) {
+    try {
+      await dynamoDBRecordHandler(record);
+    } catch (error) {
+      logger.error("Error processing record", error as Error);
+      const itemIdentifier = record.dynamodb?.SequenceNumber;
+      if (itemIdentifier) batchItemFailures.push({ itemIdentifier });
+    }
+  }
+
+  return { batchItemFailures };
+};
+
+const dynamoDBRecordHandler = async (record: DynamoDBRecord) => {
+  if (record.dynamodb && record.dynamodb.NewImage) {
+    logger.info("Processing record", { record: record.dynamodb });
+    const item = unmarshall(record.dynamodb.NewImage as { [key: string]: NativeAttributeValue });
+    logger.info("Processed item", { item });
+    items.push(item);
+  }
+};
+
+export const getProcessedItems = async () => items;
+
+export const handler = middy(dynamoDBStreamHandler).use(injectLambdaContext(logger, { logEvent: true }));
