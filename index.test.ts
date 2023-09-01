14e43de2e9a45f2c72d8656aea6b4f929aeba863
Andrew Garner <andrew.garner@cazoo.co.uk>
2023-09-01 15:03:43 +0100

    Upgrade Typescript

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "@jest/globals";
-import { dynamoDBStream } from "./index";
+import { dynamoDBStream } from "./index.js";
 
 describe("dynamoDB Streams", () => {
   it("works", () => {
